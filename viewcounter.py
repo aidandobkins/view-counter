84635bf20bf941cc6b89f0a44ce222ca4cf2508f
aidandobkins <77134910+aidandobkins@users.noreply.github.com>
2021-01-16 00:59:42 -0600

    Add files via upload

--- viewcounter.py
+++ viewcounter.py
@@ -2,20 +2,35 @@ from pyyoutube import Api
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
-
+from tkinter import *
 import urllib.parse as p
 import re
 import os
 
-api = Api(api_key='')
 sum = 0
+api = Api(api_key='AIzaSyBP8vpiJAuc8GduKZoYhqcIJHqtQlcQUKg')
 
-with open("videourls.txt", "r") as f:
-    for url in f:
-        purl = p.urlparse(url)
-        ids = p.parse_qs(purl.query).get("v")
-        print(ids[0])
-        video = api.get_video_by_id(video_id=ids)
-        sum = sum + int(video.items[0].statistics.viewCount)
+def countViews():
+    global sum
+    sum = 0
 
-print(sum)
+    with open("videourls.txt", "r") as f:
+        for url in f:
+            purl = p.urlparse(url)
+            ids = p.parse_qs(purl.query).get("v")
+            video = api.get_video_by_id(video_id=ids)
+            sum = sum + int(video.items[0].statistics.viewCount)
+    counted = Label(root, text = str(str(sum) + " views"), bg = '#e5edf5').pack()
+    
+
+root = Tk()
+root.title("View Counter")
+root.geometry("350x350")
+root.configure(bg = '#e5edf5')
+
+Label(root, text = "\n\n", bg = '#e5edf5').pack()
+count = Button(root, text = "Count Views", command = countViews, bg = '#e5edf5', pady = 50, padx = 50).pack()
+Label(root, text = "\n\n", bg = '#e5edf5').pack()
+Label(root, text = "Please give it a moment, it is slow...", bg = '#e5edf5').pack()
+
+root.mainloop()
